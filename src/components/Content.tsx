3350e6910307245cee8a214c0b42ef90229ed17b
Leandro Tavares <leandro@phox.com.br>
2021-03-17 10:42:03 -0300

    Challenge completed

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+import { MovieProps } from "../App";
+
+interface ContentProps {
+  genreId: number;
+  movies: MovieProps[];
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="movies-list">
+      {props.movies.map((movie) => (
+        <MovieCard
+          key={movie.Title}
+          title={movie.Title}
+          poster={movie.Poster}
+          runtime={movie.Runtime}
+          rating={movie.Ratings[0].Value}
+        />
+      ))}
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+import { GenreResponseProps } from "../App";
+
+interface SideBarProps {
+  id: number;
+  genres: GenreResponseProps[];
+  onClick: (id: number) => void;
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={genre.id}
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.onClick(genre.id)}
+            selected={props.id === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
